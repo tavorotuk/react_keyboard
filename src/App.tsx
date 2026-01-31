cfd50636f216544be18e235517e0f46f517573fe
Vadym Kirilkov <tav0r0tuk@gmail.com>
2026-01-31 22:08:41 +0100

    try to pass all tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  key: string;
+};
+
+export class App extends Component<State> {
+  state: State = {
+    key: '',
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.key === '' ? (
+          <p className="App__message">Nothing was pressed yet</p>
+        ) : (
+          <p className="App__message">
+            {`The last pressed key is [${this.state.key}]`}
+          </p>
+        )}
+      </div>
+    );
+  }
+}
